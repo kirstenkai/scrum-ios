ee6bcc89209341d286314de11f12c499eeb39d42
KC Kai <kirstenckai@gmail.com>
2022-10-26 00:29:15 -0400

    Initial Commit

---
+++ scrum-ios/scrum_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrum_iosApp.swift
+//  scrum-ios
+//
+//  Created by KC Kai on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct scrum_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
